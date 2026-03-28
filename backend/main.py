020b67d891f7bb538e083de19f70aa43358ddc0d
Shravya Shree Anguluri <shravyashreeanguluri@gmail.com>
2026-03-28 13:42:13 +0530

    Update the file

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from app.database import engine
-from app import models
+
 from app.routes import router
 from app.scheduler import start_scheduler
-from app.holiday.holiday_routes import router as holiday_router
 from app.docs.docs_routes import router as docs_router
-from app.timetable.timetable_routes import router as timetable_router
-from app.admin.admin_routes import router as admin_router
-
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Faculty Face Backend")
 
@@ -27,10 +21,7 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 app.mount("/certificates", StaticFiles(directory="certificates"), name="certificates")
 
 app.include_router(router)
-app.include_router(holiday_router)
 app.include_router(docs_router)
-app.include_router(timetable_router)
-router.include_router(admin_router)
 
 @app.on_event("startup")
 def startup_event():
